86c916affd64e3bf3d85373275dcdb5e60253201
Jack Cross <jmc8252@psu.edu>
2021-02-10 20:23:31 -0500

    added url for poster, about to start terminal work

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -58,6 +58,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
         
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseUrl + posterPath)
+        
+        
         return cell
     }
 
